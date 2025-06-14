adb861d473dfb3f9965ecc723ce7f6cbba9adda8
ARYAN-ark-1 <153789324+ARYAN-ark-1@users.noreply.github.com>
2025-06-14 15:12:31 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  ...(isProd && {
-    basePath: '/My-mac-portfolio',
-    assetPrefix: '/My-mac-portfolio/',
-  }),
+  trailingSlash: true, // Optional but prevents 404s
 };
 
 module.exports = nextConfig;
